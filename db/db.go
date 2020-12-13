0d5f2a6411744cb76eba7044f7e64febaaa618fb
kaan kahraman <kaankahraman@kaans-MacBook-Pro.local>
2020-12-14 02:20:24 +0300

    db init

---
+++ db/db.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	bolt "go.etcd.io/bbolt"
+	"os"
+)
+
+const dbPath = "/opt/vault/passvault.db"
+// DB represents a Bolt-backed data store.
+type DB struct {
+	*bolt.DB
+}
+
+// Tx represents a BoltDB transaction
+type Tx struct {
+	*bolt.Tx
+}
+
+// Open initializes and opens the database.
+func (db *DB) Open(path string, mode os.FileMode) error {
+	var err error
+
+	db.DB, err = bolt.Open(path, mode, nil)
+	if err != nil {
+		return err
+	}
+
+	// Create buckets.
+	err = db.Update(func(tx *Tx) error {
+		if _, err := tx.CreateBucketIfNotExists([]byte("User")); err != nil {
+			return &Error{"pages bucket error", err}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		db.Close()
+
+		return err
+	}
+
+	return nil
+}
+
+// View executes a function in the context of a read-only transaction.
+func (db *DB) View(fn func(*Tx) error) error {
+	return db.DB.View(func(tx *bolt.Tx) error {
+		return fn(&Tx{tx})
+	})
+}
+
+// Update executes a function in the context of a writable transaction.
+func (db *DB) Update(fn func(*Tx) error) error {
+	return db.DB.Update(func(tx *bolt.Tx) error {
+		return fn(&Tx{tx})
+	})
+}
